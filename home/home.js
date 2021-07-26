19721187692489287da1520fcafda54c568e87fc
satyadvaya <satyadvaya@gmail.com>
2021-07-26 11:58:01 -0700

    added eventListeners to home.js and results.js

---
+++ home/home.js
@@ -0,0 +1,16 @@
+import { getUser } from '../utils.js';
+ // import functions and grab DOM elements
+const userForm = document.getElementById('user');
+
+
+userForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const formData = new FormData(userForm);
+    const name = formData.get('name');
+    
+    const user = getUser(name);
+   
+    
+
+        window.location.replace(`../game`);
+});
