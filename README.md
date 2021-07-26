2eac0388d3250b814a782b4c2b1103eac9c01fcf
Pete Hamrick <pete.hamrick@gmail.com>
2021-07-26 13:40:38 -0700

    added some of the bootstrap template files like eslintrc gitignore and travis.yml

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ProjectWk1-Memory-game
+* Inspired by: https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript
---
+++ game/game.js
@@ -0,0 +1,6 @@
+// const gameBoard = document.getElementById('gameboard-id');
+// const counterSpan = document.getElementById('counter-span');
+
+// const tilesList = document.createElement('ul');
+// const tile = document.createElement('li');
+
