ff79d3a3483968a701125559a3995f0edc2db7ee
Pete Hamrick <pete.hamrick@gmail.com>
2021-07-26 16:12:33 -0700

    wrote a function to shuffle our pile of tiles

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ProjectWk1-Memory-game
 * Inspired by: https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript
+* also inspired by: https://github.com/UniKittyCorns/MemoryCardGame
--- game/game.js
+++ game/game.js
@@ -1,6 +1,20 @@
 // const gameBoard = document.getElementById('gameboard-id');
 // const counterSpan = document.getElementById('counter-span');
+// const tile = document.querySelector('.tile');
 
-// const tilesList = document.createElement('ul');
-// const tile = document.createElement('li');
+// Make deck
+const cardArray = ['1', '2', '3', '4', '5', '6', '7', '8'];
+// don't know if we need to push into something else in order to shuffle
 
+// shuffle deck
+const arrayCopy = cardArray.slice().sort(function(a, b) { return 0.5 - Math.random(); }); // eslint-disable-line
+const halfPile = arrayCopy.splice(0, 8);
+const fullPile = halfPile.concat(halfPile);
+// above makes a copy but they are in the same order
+const shuffledTiles = fullPile.sort(function(a, b) { return 0.5 - Math.random(); }); // eslint-disable-line
+console.log(shuffledTiles);
+
+
+// TODO
+// make tiles clickable
+// display text on tile when clicked
