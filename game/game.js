32bd6274012271669b0004c7830af8626fb32ba1
satyadvaya <satyadvaya@gmail.com>
2021-07-28 17:34:15 -0700

    made functions in game.js and random layout works

--- game/game.js
+++ game/game.js
@@ -3,7 +3,6 @@
 const gameBoard = document.getElementById('gameboard-id');
 const turnSpan = document.getElementById('counter-span');
 const matchedSpan = document.getElementById('matched-span');
-const tiles = document.querySelectorAll('.tile');
 const tileList = document.getElementById('tiles-list');
 
 // TODO
@@ -19,17 +18,52 @@ let matched = 0;
 // TODO
 // make this a function
 // shuffle deck
-const arrayCopy = tileArray.slice().sort(function(a, b) { return 0.5 - Math.random(); }); // eslint-disable-line
-const halfPile = arrayCopy.splice(0, 8);
-const fullPile = halfPile.concat(halfPile);
-// above makes a copy but they are in the same order
-const shuffledTiles = fullPile.sort(function(a, b) { return 0.5 - Math.random(); }); // eslint-disable-line
+
+//  render function
+const shuffledTiles = shuffleTiles();
+// for loop to initialize process for rendering tiles
+/*
+for (let tile of shuffledTiles) {
+    looping through, call renderTile, inside of renderTile pass tile as a parameter which will render our div and img elements
+    append that to the gameBoard
+    (1) when we call renderTile, const tileDiv = renderTile(tile)
+    (2) append tileDiv to the gameBoard
+    
+}
+*/
+
+for (let tile of shuffledTiles) {
+    const tileDiv = renderTile(tile);
+    tileDiv.classList.add(tile, 'tile');
+    gameBoard.appendChild(tileDiv);
+}
+
+function renderTile(id) {
+    const tileDiv = document.createElement('div');
+    const tileImg = document.createElement('img');
+    tileImg.classList.add('tile-face', 'tile-front');
+    tileImg.src = `../images/image${id}.jpg`;
+    // insert the id into the image
+    tileDiv.appendChild(tileImg);
+    return tileDiv;
+}
+
+function shuffleTiles() {
+    const arrayCopy = tileArray.slice().sort(function(a, b) { return 0.5 - Math.random(); }); // eslint-disable-line
+    const halfPile = arrayCopy.splice(0, 8);
+    const fullPile = halfPile.concat(halfPile);
+    // above makes a copy but they are in the same order
+    const shuffledTiles = fullPile.sort(function(a, b) { return 0.5 - Math.random(); }); // eslint-disable-line
+    // use shuffledTile array to assign an id to each div
+    return shuffledTiles;
+}
 
 // TODO
 // put this somewhere and import it
+const tiles = document.querySelectorAll('.tile');
 function tileFlip() {
     tiles.forEach(tile =>{
-        tile.addEventListener('click', ()=>{
+        tile.addEventListener('click', () => {
             tile.classList.add('noclick');
             tile.classList.toggle('flipped');
             selected.push(tile);
@@ -41,7 +75,7 @@ function tileFlip() {
                 const selected2Id = selected[1].classList.value;
                 if (selected1Id === selected2Id){
                     // setTimeout(() => {
-                    // }, 2000);
+                        // }, 2000);
                     selected[0].classList.add('noclick');
                     selected[0].classList.add('matched');
                     selected[1].classList.add('noclick');
@@ -60,16 +94,15 @@ function tileFlip() {
                         selected = [];}, 2500);
                 }
             }
-          
+                
         });
     });
 }
 const audio = new Audio('../assets/sounds/TADA.WAV');
 tileFlip();
-console.log(matched, shuffledTiles.length);
 function endGame() {
     if (matched === shuffledTiles.length / 2) {
-        // put winning sound in
+            // put winning sound in
         audio.play();
         const winSpan = document.createElement('span');
         const winDiv = document.createElement('div');
@@ -80,12 +113,12 @@ function endGame() {
         gameBoard.removeChild(tileList);
         gameBoard.appendChild(winDiv);
         setTimeout(() => { 
-            // window.location.replace('../results');
+                // window.location.replace('../results');
         }, 3500);
     }
 } 
-// update user object - grab turns,
-// end of game function to set ending conditions and set user object
+    // update user object - grab turns,
+    // end of game function to set ending conditions and set user object
 // if matched pairs = shuffed tiles/2 - end game
 // sounds and span to display win
 // update user
